3507eb4dfc9b07ad66111bd7ebcf3294f56dfc95
Education <education@Mos-MacBook-Pro.local>
2022-02-17 15:44:35 -0500

    Timer Feature

---
+++ src/features/Timer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native"
+
+export const Timer = ({ focusSubject }) => (
+  <View>
+    <Text>Focus Feature {focusSubject}</Text>
+  </View>
+)
