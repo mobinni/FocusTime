91c2b12fbb86358e1235378dfc347156b65310ff
Education <education@Mos-MacBook-Pro.local>
2022-02-17 13:25:45 -0500

    Style background

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { Text, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>Hello World!</Text>
     </SafeAreaView>
   );
 }
@@ -14,5 +16,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue
+  },
+  text: {
+    color: colors.white
   }
 });
