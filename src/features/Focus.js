b852825885169031dc5a2353993148378a43fd4f
Education <education@Mos-MacBook-Pro.local>
2022-02-17 14:51:29 -0500

    spacing

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
 import { RoundedButton } from '../components/RoundedButton';
+import { spacing } from '../utils/sizes';
 
 export const Focus = ({ addSubject }) => {
   const [subject, setSubject] = useState(null);
@@ -31,10 +32,10 @@ const styles = StyleSheet.create({
   },
   textInput: {
     flex: 1,
-    marginRight: 10,
+    marginRight: spacing.sm,
   },
   inputContainer: {
-    padding: 25,
+    padding: spacing.lg,
     justifyContent: 'top',
     flexDirection: 'row',
   },
