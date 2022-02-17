7191650c89f8f9ccb96a1741636b6a6da4dfc08f
Education <education@Mos-MacBook-Pro.local>
2022-02-17 13:48:10 -0500

    Add text input

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
 
 export const Focus = () => (
   <View style={styles.container}>
-    <Text style={styles.text}>Focus Feature</Text>
+    <View style={styles.inputContainer}>
+     <TextInput label="What would you like to focus on?" />
+    </View>
   </View>
 )
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inputContainer: {
+    flex: 0.5,
+    padding: 25,
+    justifyContent: 'top',
   },
   text: {
     color: colors.white,
