6c3d4edb2ad5fd1e7a1f04c69ff92619f5e26439
Education <education@Mos-MacBook-Pro.local>
2022-02-18 13:26:34 -0500

    focus list

--- src/features/FocusHistory.js
+++ src/features/FocusHistory.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
 
+export const FocusHistory = ({ history }) => {
+  if (!history || !history.length) return null;
 
-export const FocusHistory = () => {
+  const renderItem = ({ item }) => <Text style={styles.item}>- {item}</Text>;
 
   return (
-    <View>
-      <Text>Focus history will be built here</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on:</Text>
+      <FlatList data={history} renderItem={renderItem} />
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex:1,
+  },
+  item: {
+    fontSize: fontSizes.md,
+    color: colors.white,
+    paddingTop: spacing.sm
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+});
