8e7ff6334a47cab577a0496fa6f88b390b4aaae5
Education <education@Mos-MacBook-Pro.local>
2022-02-18 10:48:41 -0500

    add progress bar

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
 import { Countdown } from '../components/Countdown';
 import { RoundedButton } from '../components/RoundedButton';
 import { spacing } from '../utils/sizes';
 import { colors } from '../utils/colors';
+
 export const Timer = ({ focusSubject }) => {
   const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(1);
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
         <Countdown
           isPaused={!isStarted}
-          onProgress={() => {}}
+          onProgress={setProgress}
           onEnd={() => {}}
         />
         <View style={{ paddingTop: spacing.xxl }}>
           <Text style={styles.title}>Focusing on:</Text>
           <Text style={styles.task}>{focusSubject}</Text>
         </View>
+      </View>
+      <View style={{ paddingTop: spacing.sm }}>
+        <ProgressBar
+          progress={progress}
+          color={colors.progressBar}
+          style={{ height: spacing.sm }}
+        />
       </View>
       <View style={styles.buttonWrapper}>
         {!isStarted ? (
