88585ac61d9e51017957d541b71e6dbf6a1690ad
Education <education@Mos-MacBook-Pro.local>
2022-02-18 13:07:15 -0500

    setting up focus history

--- App.js
+++ App.js
@@ -11,13 +11,19 @@ import Constants from 'expo-constants';
 import { colors } from './src/utils/colors';
 import { Focus } from './src/features/Focus';
 import { Timer } from './src/features/Timer';
+import { FocusHistory } from './src/features/FocusHistory';
+
 
 export default function App() {
-  const [currentSubject, setCurrentSubject] = useState('test');
+  const [currentSubject, setCurrentSubject] = useState();
+  const [history, setHistory] = useState([]);
   return (
     <SafeAreaView style={styles.container}>
       {!currentSubject ? (
-        <Focus addSubject={setCurrentSubject} />
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
       ) : (
         <Timer 
           focusSubject={currentSubject}
