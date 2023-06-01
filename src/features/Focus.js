37a3386e71138739b90b765012e21f4e777151ca
Katie Stairs <kstairs831@gmail.com>
2023-06-01 14:02:59 -0500

    Created state for the focus input.

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native'; 
 import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
 
-export const Focus = () => (
-  <View style={styles.container}>
-    <View style={styles.inputContainer}>
-      <TextInput label="What would you like to focus on?" />
+export const Focus = () => {
+  const [subject, setSubject] = useState(null);
+  console.log(subject)
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={setSubject} label="What would you like to focus on?" />
+      </View>
     </View>
-  </View>
-)
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
