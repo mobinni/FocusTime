766c783925688bea7ef0f1e4ab10ddd0c1e50a22
Katie Stairs <kstairs831@gmail.com>
2023-06-05 14:05:38 -0500

    did some clean-up. Trying to learn how commits differ from pull requests on github.

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-
 import { fontSizes, spacing } from '../utils/sizes';
 import { colors } from '../utils/colors';
 
